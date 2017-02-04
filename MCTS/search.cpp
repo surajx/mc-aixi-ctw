b750bfcb33ea5449e1fad3dfeb93c00c7b803d60
Arie Slobbe <arie_slobbe@hotmail.com>
2017-02-04 19:06:20 +1100

    implementing search fn

--- MCTS/search.cpp
+++ MCTS/search.cpp
@@ -16,7 +16,13 @@ class SearchNode {
 
 public:
 
-	SearchNode(bool is_chance_node);
+	SearchNode(bool is_chance_node, unsigned int num_children){
+			m_visits = 0;
+			m_mean = 0;
+			m_children = num_children;
+			children.resize(m_children);
+			std::cout << "creating search node" << std::endl;
+	}
 
 	// determine the next action to play
 	action_t selectAction(Agent &agent) const; // TODO: implement
@@ -26,7 +32,13 @@ public:
 
 	// perform a sample run through this node and it's children,
 	// returning the accumulated reward from this sample run
-	reward_t sample(Agent &agent, unsigned int dfr); // TODO: implement
+	reward_t sample(Agent &agent, unsigned int dfr) {
+		std::cout << "entering sample function call " << std::endl;
+		SearchNode child(false, 2);
+		children[2] = &child;
+		std::cout << "children[2]: " << children[2] << std::endl;
+		return 0;
+	}
 
 	// number of times the search node has been visited
 	visits_t visits(void) const { return m_visits; }
@@ -37,10 +49,7 @@ private:
 	unsigned int m_children; // chance (action) nodes have |X| (|A|) children
 	double m_mean;      // the expected reward of this node
 	visits_t m_visits;  // number of times the search node has been visited
-
-
-	// TODO: decide how to reference child nodes
-	//  e.g. a fixed-size array
+	std::vector<SearchNode*> children;
 };
 
 // simulate a path through a hypothetical future for the agent within it's
@@ -49,20 +58,37 @@ static reward_t playout(Agent &agent, unsigned int playout_len) {
 	return 0; // TODO: implement
 }
 
+// initialize the tree by initializing a root node or pruning an existing tree
+void initializeTree(Agent &agent) {
+	std::cout << "initializing" << std::endl;
+	// initialize values
+	numActions = agent.numActions();
+	C = agent.exploreExploitRatio();
+	m = agent.horizon();
+	obsBits = agent.numObservations();
+	rewBits = agent.numRewards();
+	root_ptr = new SearchNode(false,numActions);
+}
+
+void pruneTree(Agent &agent) {
+	std::cout << "pruning" << std::endl;
+	SearchNode* root_ptr = new SearchNode(false,numActions);
+}
+
 // determine the best action by searching ahead using MCTS
 extern action_t search(Agent &agent) {
-	unsigned int num_simulations = agent.numSimulations();
-	unsigned int num_actions = agent.numActions();
-	double C = agent.exploreExploitRatio();
-	size_t m = agent.horizon();
-	unsigned int num_obs = agent.numObservations();
-	unsigned int num_rew = agent.numRewards();
-	SearchNode root(false); // create root (decision) node
-	// manual initialize?
-	// for i in range(num_simulations)
-	// 	prune
-	// 	sample(root, m)
+	std::cout << "lifetime: " << agent.lifetime() << std::endl;
+	if (agent.lifetime() == 0) {
+		initializeTree(agent);
+	} else {
+		pruneTree(agent);
+	}
+	SearchNode root = *root_ptr;
+	std::cout << "root.visits(): " << root.visits() << std::endl;
+	for (unsigned int sim = 1;sim <= agent.numSimulations(); sim++) {
+		std::cout << "sim: " << sim << std::endl;
+		reward_t r = root.sample(agent, 3); // test statement
+	}
 	// return bestAction(root)
-	return agent.genRandomAction(); // TODO: implement
-	//std::cout << "num_obs & num_rew: " << num_obs << " " << num_rew << std::endl;
+	return agent.genRandomAction(); // test statement
 }
--- MCTS/search.hpp
+++ MCTS/search.hpp
@@ -5,6 +5,16 @@
 
 #include "../common/types.hpp"
 
+class SearchNode;
+
+// MCTS constants
+static unsigned int numActions; // number of actions
+static unsigned int obsBits; // number of observation bits
+static unsigned int rewBits; // number of reward bits
+static double C; // exploration-exploitation constant C
+static unsigned int m; // also known as horizon, or max tree depth.
+SearchNode *root_ptr;
+
 // determine the best action by searching ahead
 extern action_t search(Agent &agent);
 
