5679f446ff323785005ac750358d851d4591fc6e
Arie Slobbe <arie_slobbe@hotmail.com>
2017-02-07 18:22:59 +1100

    implement sample. Code does not compile

--- MCTS/search.cpp
+++ MCTS/search.cpp
@@ -30,11 +30,10 @@ public:
 	}
 
 	// return unif random one of the best actions
-	action_t selectAction(Agent &agent) const {
-		// collect set of best children and return unif randomly one action from set
-		std::vector<int> best_children;
+	action_t bestAction(Agent &agent) const {
+		std::vector<action_t> best_children;
 		best_children.push_back(0);
-		unsigned int best_mean = children[0]->expectation();
+		double best_mean = children[0]->expectation();
 		unsigned int num_best_children = 1;
 		for (int i = 1; i < m_children;i++) {
 			if (children[i] != NULL) {
@@ -59,29 +58,80 @@ public:
 		return best_children[random_number];
 	}
 
-	// determine the expected reward from this node
-	reward_t expectation(void) const { return m_mean; }
+	// select action based on UCT algorithm
+	action_t selectAction(Agent &agent, int test_arg) const {
+		// START test code generate action
+		action_t a;
+		if (test_arg == 0) {
+			a = 0;
+		} else if (test_arg == 1) {
+			a = 1;
+		} else {
+			std::cout << "h wrongly identified as previously visited action node" << std::endl;
+			a = 0;
+		}
+		// create child a
+		SearchNode ha(true, numPercepts);
+		children[a] = &ha;
+		return a;
+		// END test code generate action
 
-	// perform a sample run through this node and it's children,
+		// START pseudocode
+		// make set of unvisited
+		// if unvisited nonempty then
+		//   for unvisited
+		//     pick unif random a
+		//     create ha
+		//     return a
+		// else
+		//   return argmax
+		// END pseudocode
+	}
+
+
+	// perform a sample run through this node and its children,
 	// returning the accumulated reward from this sample run
-	reward_t sample(Agent &agent, unsigned int dfr) {
-		std::cout << "entering sample function call " << std::endl;
-		SearchNode child0(true, 2);
-		SearchNode child1(true, 2);
-		SearchNode child2(true, 2);
-		children[0] = &child0;
-		children[1] = &child1;
-		children[2] = &child2;
-		children[0]->m_mean = 4;
-		children[1]->m_mean = 4;
-		children[2]->m_mean = 4;
-		std::cout << "children[1]->expectation(): " << children[1]->expectation() << std::endl;
-		std::cout << "children[2]: " << children[2] << std::endl;
-		return 0;
+	reward_t sample(Agent &agent, unsigned int horiz, int test_arg) {
+		std::cout << "horiz: " << horiz << std::endl;
+		std::cout << "m: " << m << std::endl;
+		reward_t rew = 0;
+		if (horiz == 0) {
+			return rew;
+		} else if (m_chance_node) {
+			// START test code generate percept
+			percept_t obs_rew;
+			if (test_arg == 2) {
+				obs_rew = 0;
+			} else if (test_arg == 3) {
+				obs_rew = 1;
+			} else {
+				std::cout << "h wrongly identified as chance node" << std::endl;
+				return 0;
+			}
+			// END test code generate percept
+			hor_ptr = new SearchNode(false,numActions);
+			if (children[obs_rew] != NULL) { // if child (o,r) does not exist:
+				hor_ptr = children[obs_rew];
+			}
+			rew = rew + hor_ptr->sample(Agent &agent, horiz - 1, test_arg+1);
+		} else if (m_visits == 0) {
+			rew = playout(agent, horiz);
+		} else {
+			SearchNode ha = selectAction(Agent &agent, test_arg+1);
+			rew = ha.sample(agent, m, test_arg+1);
+		}
+		// update m_mean
+		m_mean =  (1 / m_visits) * (rew + m_visits*m_mean);
+		// update visits
+		m_visits++;
+		return rew;
 	}
 
 	// number of times the search node has been visited
 	visits_t visits(void) const { return m_visits; }
+
+	// determine the expected reward from this node
+	reward_t expectation(void) const { return m_mean; }
 
 private:
 
@@ -92,10 +142,10 @@ private:
 	std::vector<SearchNode*> children;
 };
 
-// simulate a path through a hypothetical future for the agent within it's
+// simulate a path through a hypothetical future for the agent within its
 // internal model of the world, returning the accumulated reward.
 static reward_t playout(Agent &agent, unsigned int playout_len) {
-	return 0; // TODO: implement
+	return 1;
 }
 
 // number of distinct percepts (size of the percept space)
@@ -106,10 +156,10 @@ unsigned int countPercepts(unsigned int num_obs, unsigned num_rew) {
 // initialize the tree by setting the constants and creating a root node
 void initializeTree(Agent &agent) {
 	std::cout << "initializing" << std::endl;
-	// initialize values
 	// numActions = agent.numActions(); // set number of actions
+	// numPercepts = countPercepts(agent.numObservations(), agent.numRewards()); // set number of percepts
 	numActions = 3; // test
-	numPercepts = countPercepts(2,2);
+	numPercepts = countPercepts(2,2); // test
 	C = agent.exploreExploitRatio();
 	m = agent.horizon();
 	obsBits = agent.numObservations();
@@ -125,6 +175,7 @@ void pruneTree(Agent &agent) {
 
 // determine the best action by searching ahead using MCTS
 extern action_t search(Agent &agent) {
+	std::cout << "test of 1/10: " << 1/10 << std::endl;
 	std::cout << "lifetime: " << agent.lifetime() << std::endl;
 	if (agent.lifetime() == 0) {
 		initializeTree(agent);
@@ -135,8 +186,8 @@ extern action_t search(Agent &agent) {
 	std::cout << "root.expectation(): " << root.expectation() << std::endl;
 	for (unsigned int sim = 1;sim <= agent.numSimulations(); sim++) {
 		std::cout << "sim: " << sim << std::endl;
-		reward_t r = root.sample(agent, 3); // test statement
+		reward_t r = root.sample(agent, m, 0); // test statement
 	}
-	return root.selectAction(agent);
+	return root.bestAction(agent);
 	//return agent.genRandomAction(); // test statement
 }
